31c53b05c54930d0c0b765877168fd3fcbf2de82
Gustavo <gustavo.salazar@ravn.co>
2025-04-14 15:50:52 -0500

    feat(type-excercises):Excercises finished and examples optimized

--- src/type-exercises.ts
+++ src/type-exercises.ts
@@ -22,22 +22,27 @@
 
 // 1.1 Add here your solution:
 type OmitByType<Type, Property> = {
-  [K in keyof Type as Type[K] extends Property ? never : K]: Type[K]
+    [K in keyof Type as Type[K] extends Property ? never : K]: Type[K]
 }
 
 // 1.2 Add here your example:
-type example1 = {
-  id: number
-  name: string
-  active: boolean
+type AdminPrivilegue = {
+    id: number
+    name: string
+    active: boolean
+    privilege: symbol
+
 }
 
-type OmitNumber = OmitByType<example1, number>
+type NormalUser = OmitByType<AdminPrivilegue, symbol>
 
-/** 1.3 This example will return:
- *  type OmitNumber = {
- * name: string;
- * active: boolean}
+/** 1.3 This example will return this type very useful to remove properties:
+ *  type NormalUser = {
+ *  id: number
+ *  name: string;
+ *  active: boolean}
+ /*
+
 
 /**
  * Exercise #2: Implement the utility type `If<C, T, F>`, which evaluates a condition `C`
@@ -56,11 +61,30 @@ type OmitNumber = OmitByType<example1, number>
 
 // 2.1 Add here your solution:
 type If<condition extends boolean, TrueArg, FalseArg> = condition extends true
-  ? TrueArg
-  : FalseArg
+    ? TrueArg
+    : FalseArg
+
 // 2.2 Add here your example:
-type IsTrue = If<true, 'Yes', 'No'> //this wil return type IsTrue = "Yes"
-type IsFalse = If<false, 'Yes', 'No'> //this wil return type IsFalse = "No"
+type Usertype<IsPremium extends boolean> = {
+    name: string;
+    type: If<IsPremium, "premium", "normal">;
+};
+
+type UserPremium = Usertype<true>; // type: "premium"
+type UserNormal = Usertype<false>; // type: "normal"
+
+/**
+ * 2.3 This type is very useful to change privileges among users using just one type:
+ 
+ * type UserPremium = {
+    name: string;
+    type: "premium";
+}
+    type UserNormal = {
+    name: string;
+    type: "normal";
+}  
+ */
 
 /**
  * Exercise #3: Recreate the built-in `Readonly<T>` utility type without using it.
@@ -86,13 +110,13 @@ type IsFalse = If<false, 'Yes', 'No'> //this wil return type IsFalse = "No"
 
 // 3.1 Add here your solution
 type MyReadonly<T> = {
-  readonly [K in keyof T]: T[K]
+    readonly [K in keyof T]: T[K]
 }
 // 3.2 Add here your example
 type plants = {
-  name: string
-  water: boolean
-  sunlight: boolean
+    name: string
+    water: boolean
+    sunlight: boolean
 }
 
 type MyReadonlyplant = MyReadonly<plants>
@@ -103,6 +127,8 @@ type MyReadonlyplant = MyReadonly<plants>
      readonly name: string;
      readonly water: boolean;
      readonly sunlight: boolean}
+     
+     very important to protect the type from mutability.
  */
 
 /**
@@ -127,14 +153,23 @@ type MyReadonlyplant = MyReadonly<plants>
 type MyReturnType<T> = T extends (...args: any[]) => infer R ? R : never
 
 // 4.2 Add here your example:
-function sumar(a: number, b: number): number {
-  return a + b
+function getuserinfo(id: number) {
+    return {
+        id,
+        name: "Gustavo",
+        active: true,
+    };
 }
 
-type Resultado = MyReturnType<typeof sumar>
-/**This example will return in compilation time:
- * type Resultado = number
- */
+type InfoUser = MyReturnType<typeof getuserinfo>
+/** 4.3 This example will return de information of a user API callback example:
+ *  {
+     id: number;
+     name: string;
+     active: boolean;
+    }
+*/
+
 
 /**
  * Exercise #5: Extract the type inside a wrapped type like `Promise`.
@@ -154,11 +189,14 @@ type Resultado = MyReturnType<typeof sumar>
 type MyAwaited<T> = T extends Promise<infer U> ? U : null
 
 // 5.2 Add here your example:
-type IhaveaPromise = Promise<string>
-type IdonthaveaPromise = 3
+async function obtainName(): Promise<string> { //This will simulate an API call
+    return "Alonso";
+}
 
-type Result1 = MyAwaited<IhaveaPromise> // This will return the promise type string.
-type Result2 = MyAwaited<IdonthaveaPromise> // This will return null since there is no promise.
+type ResultInfo = MyAwaited<ReturnType<typeof obtainName>>;
+/**5.3 The ResulInfo will return "string" very useful to obtain 
+ * which type of data we are calling from an API.
+/*
 
 /**
  * Exercise 6: Create a utility type `RequiredByKeys<T, K>` that makes specific keys of `T` required.
@@ -183,22 +221,25 @@ type Result2 = MyAwaited<IdonthaveaPromise> // This will return null since there
 
 // 6.1 Add here your solution:
 type RequiredByKeys<T, K extends keyof T> = T &
-  { [Prop in K]-?: T[Prop] } extends infer Combined
-  ? {
-      [Prop in keyof Combined]: Combined[Prop]
+{ [Prop in K]-?: T[Prop] } extends infer Combined
+    ? {
+        [Prop in keyof Combined]: Combined[Prop]
     }
-  : never
+    : never
 
 // 6.2 Add here your example:
-interface ExampleUser {
-  name: string
-  age?: number
-  address?: string
-}
-type newExample = RequiredByKeys<ExampleUser, 'name'>
+interface FormContact {
+    name?: string;
+    email?: string;
+    phone?: string;
+  }
+type Validform = RequiredByKeys<FormContact, 'name'>
 /**
- * 6.3 This will return type newExample = {
- * name: string;
- * age?: number | undefined;
- * address?: string | undefined;
+ * 6.3 This will return type
+ * type Validform = {
+    name: string;
+    email?: string | undefined;
+    phone?: string | undefined;
+    }
+ * Very important if we need some required information in a form example
  */
