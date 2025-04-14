715cb0d55d492ed72a5546dd88724dcfa45d4f56
Gustavo <gustavo.salazar@ravn.co>
2025-04-14 10:23:07 -0500

    feat(app):function getTemperatureSummary completed

--- src/app.ts
+++ src/app.ts
@@ -13,22 +13,36 @@ interface TemperatureSummary {
   average: number
 }
 
-
-
 export function processReadings(readings: TemperatureReading[]): void {
   // add here your code
-  console.log("Process readings...")
-  readings.forEach(reading => {readings.push(reading)})
+  readings.forEach((reading) => {
+    storedReadings.push(reading)
+  })
 }
 
-const readings: TemperatureReading [] =[]
-
+const storedReadings: TemperatureReading[] = []
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
 ): TemperatureSummary | null {
   //add here your code
-  
-  return null
+  const targetDate = date.toISOString().slice(0, 10)
+  const result = storedReadings.filter(
+    (reading) =>
+      reading.time.toISOString().slice(0, 10) == targetDate &&
+      reading.city == city,
+  )
+
+  if (result.length === 0) {
+    return null
+  }
+
+  const first = result[0].temperature
+  const last = result[result.length - 1].temperature
+  const temperatures = result.map((r) => r.temperature)
+  const high = Math.max(...temperatures)
+  const low = Math.min(...temperatures)
+  const average = temperatures.reduce((acc, temp) => acc + temp, 0) / temperatures.length
+  return { first, last, high, low, average }
 }
